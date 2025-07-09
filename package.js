136b3ba89cbbde46967589b9bc513fb6a79b6d0f
triplem01 <couragenkoka6@gmail.com>
2025-07-09 12:39:20 +0200

    package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "Knigh SlayerV1",
+  "version": "1.0.0",
+  "scripts": {
+    "start": "node app.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "twilio": "^3.71.0",
+    "body-parser": "^1.19.0"
+  }
+}
